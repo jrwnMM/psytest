d3cf906c7877f124f5be72b2051de33325d892e8
JerwinCarreon <jcarreon@jairosolutions.com>
2022-06-22 21:00:50 +0800

    fixed dataprivacy consent redirect

--- psytests/settings/prod.py
+++ psytests/settings/prod.py
@@ -13,3 +13,7 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=500, ssl_require=True)
 }
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
