6feaca7fc89601d7bb31132974e816ea34dbf84d
jerwin <jerwin.carreon@jmc.edu.ph>
2023-02-18 13:21:37 +0800

    debug

--- psytests/settings/prod.py
+++ psytests/settings/prod.py
@@ -2,11 +2,12 @@ from .common import *
 import dj_database_url
 
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = ["psytest-6ll2.onrender.com", "https://psytest-6ll2.onrender.com/"]
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-SITE_ID = 3
+SITE_ID = 1
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500, ssl_require=True)
